091d6e1fa5a6cbf8bd9328159069a0a7122708af
Angela Basnet <angellabasnet@gmail.com>
2025-03-04 10:28:49 +0545

    display two dec

--- src/components/Tip.jsx
+++ src/components/Tip.jsx
@@ -1,111 +1,101 @@
 import styled from "styled-components";
 
 const TipBody = styled.div`
-    display: flex;
-    flex-direction: column;
-    row-gap: 15px;
-`
+  display: flex;
+  flex-direction: column;
+  row-gap: 15px;
+`;
 
 const Title = styled.p`
-    color: var(--dark-grayish-cyan);
-    font-size: 15px;
-`
+  color: var(--dark-grayish-cyan);
+  font-size: 15px;
+`;
 const Buttons = styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 0.5rem;
-
-`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 0.5rem;
+`;
 
 const Button = styled.button`
-    width: 115px;
-    height: 48px;
-    background-color: var(--very-dark-cyan);
-    color: var(--white);
-    border: none;
-    border-radius: 5px;
-    font-size: 18px;
+  width: 115px;
+  height: 48px;
+  background-color: var(--very-dark-cyan);
+  color: var(--white);
+  border: none;
+  border-radius: 5px;
+  font-size: 18px;
 
-    
-    &:hover{
-        background-color: #9fe8df;
-        color: var(--very-dark-cyan);
-        cursor: pointer;
-    }
-
-`
+  &:hover {
+    background-color: #9fe8df;
+    color: var(--very-dark-cyan);
+    cursor: pointer;
+  }
+`;
 const ButtonCustom = styled.input`
-    width: 115px;
-    height: 48px;
-    background-color: var(--very-light-grayish-cyan);
-    color: var(--dark-grayish-cyan);
-    border: 2px solid var(--very-light-grayish-cyan);
-    border-radius: 5px;
-    outline: none;
-    font-size: 18px;
-    text-align: right;
-    caret-color: var(--strong-cyan);
-    
-    &:hover{
-        cursor: pointer;
-    }
+  width: 115px;
+  height: 48px;
+  background-color: var(--very-light-grayish-cyan);
+  color: var(--dark-grayish-cyan);
+  border: 2px solid var(--very-light-grayish-cyan);
+  border-radius: 5px;
+  outline: none;
+  font-size: 18px;
+  text-align: right;
+  caret-color: var(--strong-cyan);
 
-    &:focus{
-        border: 2px solid var(--strong cyan);
-    }
-`
+  &:hover {
+    cursor: pointer;
+  }
 
-const Tip = ({bill, tip, number, setTipAmt, setTotal})=>{
+  &:focus {
+    border: 2px solid var(--strong cyan);
+  }
+`;
 
-    const createTip = (e)=>{
-        console.log(e.target.value)
-        const tippercent = e.target.value/100
-        console.log(bill)
-        tip = bill*tippercent
-        console.log(tip)
-        const tipamount = tip/number
-        console.log(tipamount)
-        setTipAmt(tipamount)
-        createTotal()
-    }
+const Tip = ({ bill, tip, number, setTipAmt, setTotal }) => {
+  const createTip = (e) => {
+    console.log(e.target.value);
+    const tippercent = e.target.value / 100;
+    console.log(bill);
+    tip = bill * tippercent;
+    console.log(tip);
+    const tipamount = (tip / number).toFixed(2);
+    console.log(tipamount);
+    setTipAmt(tipamount);
+    createTotal();
+  };
 
-    const createTotal = ()=>{
-        const totalbill = Number(bill)+Number(tip)
-        console.log(totalbill)
-        const totalperperson = totalbill/number 
-        setTotal(totalperperson)
-        console.log(totalperperson)
-        
-    }
+  const createTotal = () => {
+    const totalbill = Number(bill) + Number(tip);
+    console.log(totalbill);
+    const totalperperson = (totalbill / number).toFixed(2);
+    setTotal(totalperperson);
+    console.log(totalperperson);
+  };
 
+  return (
+    <TipBody>
+      <Title>Select Tip %</Title>
+      <Buttons>
+        <Button value={5} onClick={createTip}>
+          5%
+        </Button>
+        <Button value={10} onClick={createTip}>
+          10%
+        </Button>
+        <Button value={15} onClick={createTip}>
+          15%
+        </Button>
+        <Button value={25} onClick={createTip}>
+          25%
+        </Button>
+        <Button value={50} onClick={createTip}>
+          50%
+        </Button>
+        <ButtonCustom type="number" placeholder="Custom" onChange={createTip} />
+      </Buttons>
+    </TipBody>
+  );
+};
 
-    return(
-        <TipBody>        
-            <Title>Select Tip %</Title>
-              <Buttons>
-                <Button
-                value={5}
-                onClick={createTip}>5%</Button>
-                <Button
-                value={10}
-                onClick={createTip}>10%</Button>
-                <Button
-                value={15}
-                onClick={createTip}>15%</Button>
-                <Button
-                value={25}
-                onClick={createTip}>25%</Button>
-                <Button
-                value={50}
-                onClick={createTip}>50%</Button>
-                <ButtonCustom
-                type="number"
-                placeholder="Custom"
-                onChange={createTip}
-                />
-              </Buttons>
-        </TipBody>
-    ) 
-}
-
-export default Tip
+export default Tip;
